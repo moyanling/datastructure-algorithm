457479c91ae6382047931f40646b0bd704671f6a
mo39.fmbh@gmail.com <mo39.fmbh@gmail.com>
2018-05-02 14:03:05 -0400

    Two more problems

--- src/main/scala/org/mo39/fmbh/datastructure/string/AddStrings.scala
+++ src/main/scala/org/mo39/fmbh/datastructure/string/AddStrings.scala
@@ -1,29 +1,30 @@
 package org.mo39.fmbh.datastructure.string
 
-object AddStrings {
+import org.mo39.fmbh.commons.utils.Enumerable
 
-  sealed trait AddStrings {
-    def addStrings(num1: String, num2: String): String
-  }
+sealed trait AddStrings {
+  def addStrings(num1: String, num2: String): String
+}
+
+object AddStrings extends Enumerable[AddStrings] {
 
   /**
-   * Take care of the type of variable. Char.toInt method returns the ASCII code.
-   */
-  case object SOLUTION extends AddStrings {
+    * Take care of the type of variable. Char.toInt method returns the ASCII code.
+    */
+  case object Solution extends AddStrings {
     override def addStrings(num1: String, num2: String): String = {
-      var (i, carry, result) = (0, 0, new StringBuilder)
+      var (i, carry) = (0, 0)
+      val result = new StringBuilder
+      val getDigit = (s: String, i: Int) =>
+        if (i < s.length) s(s.length - 1 - i).asDigit else 0
       while (i < num1.length || i < num2.length || carry != 0) {
-        val v = (if (i < num1.length) num1(num1.length - 1 - i) - '0' else 0) + (if (i < num2.length) num2(num2.length - 1 - i) - '0' else 0) + carry
+        val v = getDigit(num1, i) + getDigit(num2, i) + carry
         result.append(v % 10)
         carry = v / 10
         i += 1
       }
       result.reverse.toString
     }
-  }
-
-  def main(args: Array[String]): Unit = {
-    println(SOLUTION.addStrings("10001", "6789"))
   }
 
 }
